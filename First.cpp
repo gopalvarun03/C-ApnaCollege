bdc33d8fe7b9dbdd012999ddb004774b0e440e55
Varun Gopal <gopalvarun03@gmail.com>
2023-05-31 17:45:44 +0530

    Day1
    ADone

--- First.cpp
+++ First.cpp
@@ -1,8 +1,44 @@
 #include<iostream>
-using namespace std;
+#include<string>
+// using namespace std;
+using std::cout;
+using std::cin;
+class MyClass
+{
+    public:
+        int height;
+        void display()
+        {
+            cout<<height;
+        }
+};
 int main()
 {
-    cout<<"Hello World"<<endl<<endl;
+    MyClass m;
+    int a;
+    m.height=50;
+    m.display();
+    cin>>a;
+    const std::string str="Hello";
+    cout<<std::endl<<"Hello World"<<std::endl<<str<<std::endl;
     return 0;
-    
 }
+/*
+
+namespace s1
+{
+    void sort()
+    {
+        cout<<"Hello";
+    }
+}
+namespace s2
+{
+    void sort()
+    {
+        cout<<"Hey";
+    }
+}
+
+
+*/
