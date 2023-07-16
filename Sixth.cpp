064013d9705b645bdfe1d0ac46d91a4abd10131d
gopalvarun03 <gopalvarun03@gmail.com>
2023-07-16 12:09:45 +0530

    a

--- Sixth.cpp
+++ Sixth.cpp
@@ -1,39 +1,27 @@
 #include<iostream>
 using namespace std;
-void prarr(int n,int arr[])
-{
-    for(int i=0;i<n;i++)
-    cout<<arr[i]<<" ";
-    cout<<endl;
-}
-void answer(int n,int arr[],int pr)
-{
-    if(n==1)
-    {
-        arr[pr]=1;
-        prarr(pr+1,arr);
-    }
-    else if(n==2)
-    {
-        arr[pr]=2;
-        prarr(pr+1,arr);
-        arr[pr]=arr[pr+1]=1;
-        prarr(pr+2,arr);
-    }
-    else{
-        answer(1,arr,pr);
-        answer(n-1,arr,pr+1);
-        answer(2,arr,pr);
-        answer(n-2,arr,pr+1);
-
-    }
-}
+int m1(int);
+int m2(int);
+int m3(int);
 int main()
 {
-    cout<<"Enter the number of steps in stair case: ";
+    cout<<"Enter the number of steps to reach to the top: ";
     int n;
     cin>>n;
-    int arr[n];
-    answer(n,arr,0);
+    m1(n);
+    m2(n);
+    m3(n);
     return 0;
+}
+int m1(int n)
+{
+
+}
+int m2(int n)
+{
+    
+}
+int m3(int n)
+{
+    
 }
