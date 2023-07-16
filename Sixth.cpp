8886c93676b9edf12acf64be3c03de9bfcb7b4bf
gopalvarun03 <gopalvarun03@gmail.com>
2023-07-16 12:27:21 +0530

    a

--- Sixth.cpp
+++ Sixth.cpp
@@ -5,23 +5,46 @@ int m2(int);
 int m3(int);
 int main()
 {
-    cout<<"Enter the number of steps to reach to the top: ";
+    cout<<"Enter the number of steps to reach to the top using 1,2 or 3 steps: ";
     int n;
     cin>>n;
-    m1(n);
-    m2(n);
+    cout<<"Varun's method: "<<m1(n)<<endl;
+    cout<<"Recursion method(better): "<<m2(n)<<endl;
+    cout<<"DP: "<<endl;
     m3(n);
     return 0;
 }
 int m1(int n)
 {
-
+    if(n==1)
+    {
+        return 1;
+    }
+    else if(n==2)
+    {
+        return 2;
+    }
+    else if(n==3)
+    {
+        return 4;
+    }
+    else{
+        return m1(n-1)+m1(n-2)+m1(n-3);
+    }
 }
 int m2(int n)
 {
+        if (n == 0)
+            return 1;
+        else if (n < 0)
+            return 0;
+ 
+        else
+            return m2(n - 3) + m2(n - 2)
+                   + m2(n - 1);
     
 }
 int m3(int n)
 {
-    
+    return 0;
 }
